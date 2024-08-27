3752dc2b4452dfa96b181e142819e2f8aed26628
Lucas <lucasefernandes333@gmail.com>
2024-08-27 20:58:41 -0300

    chore: Fix docker-compose volumes & normalize EOL

--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -1,9 +1,13 @@
+# init-db.sh
 #!/bin/bash
 set -e
 
-if [ -z "$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\dt' | grep -v 'No relations found.')" ]; then
-  echo "Database is empty. Importing dump.sql..."
+DUMP_APPLIED_FLAG="/var/lib/postgresql/data/dump_applied"
+
+if [ ! -f "$DUMP_APPLIED_FLAG" ]; then
+  echo "Database is empty or dump not applied. Importing dump.sql..."
   psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/dump.sql
+  touch "$DUMP_APPLIED_FLAG"
 else
-  echo "Database is not empty. Skipping import."
+  echo "Dump already applied. Skipping import."
 fi
